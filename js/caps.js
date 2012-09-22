e520b43864750a12f23c1df2afd89d64bbd9ac4b
outadoc <outadoc@me.com>
2012-09-22 08:17:21 +0200

    Well hmm, looks like I don't know my months

--- js/caps.js
+++ js/caps.js
@@ -1,6 +1,7 @@
 var date = new Date();
 
-if((date.getDate() == 22 && date.getMonth() == 9-1) || (date.getDate() == 28 && date.getMonth() == 6-1)) {
+//if october 22th or june 28th
+if((date.getDate() == 22 && date.getMonth() == 10-1) || (date.getDate() == 28 && date.getMonth() == 6-1)) {
 	$('*').css('text-transform', 'uppercase');
 	
 	$('input, textarea').change(function() {
